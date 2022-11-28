175dd2378181578db88aa1d392f337b4ac747d2c
PhucLee14 <lhphucth14@gmail.com>
2022-11-28 23:34:53 +0700

    add modal order newspaper

---
+++ js/script.js
@@ -0,0 +1,79 @@
+function Validator(options) {
+
+    function validate(inputElement, rule) {
+        var errorElement = inputElement.parentElement.querySelector('.form-error');
+        var errorMessage = rule.test(inputElement.value);
+                    
+        if(errorMessage) {
+            errorElement.innerText = errorMessage;
+            inputElement.parentElement.classList.add('invalid');
+        }
+        else {
+            errorElement.innerText = '';
+            inputElement.parentElement.classList.remove('invalid');   
+        }
+    }
+
+    var formElement = document.querySelector(options.form);
+
+    if(formElement) {
+        options.rules.forEach(function(rule) {
+            var inputElement = formElement.querySelector(rule.selector);
+            
+            if(inputElement) {
+                inputElement.onblur = function() {
+                    validate(inputElement, rule);
+                }
+
+                inputElement.oninput = function() {               
+                    var errorElement = inputElement.parentElement.querySelector('.form-error');
+                    errorElement.innerText = '';
+                    inputElement.parentElement.classList.remove('invalid'); 
+                }
+            }
+        });
+    }
+}
+
+Validator.isName = function(selector) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value ? undefined : 'Please input your name';
+        }
+    }
+}
+
+Validator.isContact = function(selector) {
+    return {
+        selector: selector,
+        test: function(value) {
+            var regex_mail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            var regex_number = /^[0-9]*[1-9]+$|^[1-9]+[0-9]*$/;
+            if(regex_mail.test(value) || regex_number.test(value)) {
+                undefined;
+            }
+            else {
+                return 'Please input your email or your phone number';
+            }
+        }
+    };
+}
+
+Validator.isAddress = function(selector) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value ? undefined : 'Please input your address';
+        }
+    }
+}
+
+Validator.isQuantity = function(selector) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value ? undefined : 'Please input the number of newspapers you want';
+        }
+    }
+}
